c4e0c32ff6c171c8ff6c92e3c3e44bc2cd3f6e40
rroldan <roldan.ricardo@gmail.com>
2023-10-04 07:45:34 +0200

    add navBard

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
-<main>
-</main>
-
-<style>
-</style>
+<Navbar rounded color="form">
+  <NavBrand href="/">
+    <img src="logo.jpg" class="mr-3 h-6 sm:h-9" alt="logo_beauty_by_diananaya" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Beauty by Diana Anaya</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/contact">Contacto</NavLi>
+  </NavUl>
+</Navbar>
