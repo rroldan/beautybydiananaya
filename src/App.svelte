67b00fd81543657bac01b0a0aab01265d3511060
rroldan <roldan.ricardo@gmail.com>
2023-10-07 21:33:08 +0200

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,56 @@
 <script lang="ts">
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  import { Footer, FooterBrand, FooterCopyright, FooterIcon, FooterLink, FooterLinkGroup } from 'flowbite-svelte';
+  import { Section, Portfolio, PortfolioItem } from 'flowbite-svelte-blocks';
 </script>
+<maim>
+<div class="flex flex-col h-screen justify-between">
+  <header></header>
+<Footer>
+  <div class="md:flex md:justify-between">
+    <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+        <FooterLinkGroup>
+          <FooterLink liClass="mb-4" href="/">
+            <p class="mb-4 flex items-center justify-center md:justify-start">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="mr-3 h-5 w-5">
+            <path
+              d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+            <path
+              d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+          </svg> C/ Pan y Toros, 6 - Madrid</p>
+          </FooterLink>
+          <FooterLink liClass="mb-4" href="/">
+            <p class="mb-4 flex items-center justify-center md:justify-start">
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="mr-3 h-5 w-5">
+              <path
+                fill-rule="evenodd"
+                d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                clip-rule="evenodd" />
+            </svg> +34 626 38 41 41</p>
+          </FooterLink>
+          <FooterLink liClass="mb-4" href="/">
+            <p class="mb-4 flex items-center justify-center md:justify-start">
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="mr-3 h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path
+                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+              </svg> @beauty_by_diananaya</p>
+          </FooterLink>
+        </FooterLinkGroup>
+      </div>
+  </div>
+</Footer>
+</div>
+</maim>
 
-<Navbar rounded color="form">
-  <NavBrand href="/">
-    <img src="logo.jpg" class="mr-3 h-6 sm:h-9" alt="logo_beauty_by_diananaya" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Beauty by Diana Anaya</span>
-  </NavBrand>
-  <NavHamburger />
-  <NavUl>
-    <NavLi href="/" active={true}>Home</NavLi>
-    <NavLi href="/contact">Contacto</NavLi>
-  </NavUl>
-</Navbar>
